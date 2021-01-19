67bf0d5c239c3ef9ca0d80afe090e61444f1d141
aldy-san <nsmaulidi12@gmail.com>
2021-01-20 06:43:29 +0700

    Initial commit

---
+++ application/views/home.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>./assets/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 class="text-blue-500">aaaaaaaaaa</h1>
+</body>
+
+</html>
